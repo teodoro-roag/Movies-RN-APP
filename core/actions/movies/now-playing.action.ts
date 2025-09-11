be548f5fa3d2e8e058887215317108fb0673080f
Teodoro Rodriguez Aguirre <teodororoag@gmail.com>
2025-09-11 09:03:31 -0600

    Creamos una interfaz para indicarle a ts que la información de la API va a lucir de cierta manera

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,12 +1,15 @@
 import { movieApi } from '@/core/api/movie-api';
+import { MovieDBMoviesResponse } from '@/infrastructure/interfaces/moviedb-responses';
 
 // Acción para obtener las películas que están en cartelera actualmente.
 export const nowPlayingAction = async () => {
   
   try{
 
-    const { data } = await movieApi.get('/now_playing');
-    console.log(JSON.stringify(data, null, 2));
+    const { data } = await movieApi.get<MovieDBMoviesResponse>('/now_playing');
+    // console.log(JSON.stringify(data, null, 2));
+
+    
 
     return [];
 
