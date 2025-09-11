71fb4910bee55ae23981cf91a75c6151e14ad8c7
Teodoro Rodriguez Aguirre <teodororoag@gmail.com>
2025-09-11 06:33:25 -0600

    Se realizó la instalacion y configuración de NativeWind, ademas se borraron archivos innecesarios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Text, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import "../global.css";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Root Layout
+      </Text>
+    </View>
+  )
 }
+
+export default RootLayout
