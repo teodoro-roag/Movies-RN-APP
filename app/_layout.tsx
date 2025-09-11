93b60ab792a411a731e7c78ee9553449e37a25d0
Teodoro Rodriguez Aguirre <teodororoag@gmail.com>
2025-09-11 06:50:11 -0600

    Se agregaron variables de entorno y API Key

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import "../global.css";
 const RootLayout = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-3xl font-bold text-blue-500">
         Root Layout
       </Text>
     </View>
