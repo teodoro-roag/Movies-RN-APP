6dcf9a49f81ac718c1b71df537db80d865d24015
Teodoro Rodriguez Aguirre <teodororoag@gmail.com>
2025-09-19 13:39:48 -0600

    Se creo la pantalla para mostrar peliculas con una estructura en blanco, además se lanza el onPress para ir desde el home a la ventana de la pelicula seleccionada pasando el id

---
+++ app/movie/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const movieScreen = () => {
+  return (
+    <View>
+      <Text>movieScreen</Text>
+    </View>
+  )
+}
+
+export default movieScreen
--- presentation/components/movies/MoviePoster.tsx
+++ presentation/components/movies/MoviePoster.tsx
@@ -1,3 +1,4 @@
+import { router } from 'expo-router';
 import { Image, Pressable } from 'react-native';
 
 interface Props {
@@ -11,6 +12,7 @@ const MoviePoster = ({ id, poster, smallPoster = false, className }: Props) => {
   return (
     <Pressable
       className= {`active:opacity-90 px-2 ${className}`}
+      onPress= { () => router.push(`/movie/${ id }`) }
     >
       <Image 
         source={{ uri: poster }} 
