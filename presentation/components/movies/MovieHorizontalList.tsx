bbff74164555ca0aeaa1fe33ee1b9b5d7fe3e96e
Teodoro Rodriguez Aguirre <teodororoag@gmail.com>
2025-09-19 13:08:38 -0600

    Lógica para determinar fin de FlatList de MovieHorizontalList.tsx

--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -1,5 +1,6 @@
 import { Movie } from '@/infrastructure/interfaces/movie.interface';
-import { FlatList, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { FlatList, NativeScrollEvent, NativeSyntheticEvent, Text, View } from 'react-native';
 import MoviePoster from './MoviePoster';
 
 
@@ -7,23 +8,49 @@ interface Props {
   title?: string;
   movies: Movie[];
   className?: string;
+
+  loadNextPage?: () => void;
 }
 
-const MovieHorizontalList = ({ title, movies, className }: Props) => {
-  return (
-    <View className={`${className}`}>
-      { title && <Text className="text-3xl font-bold px-4 mb-2">{title}</Text>}
+const MovieHorizontalList = ( { title, movies, className, loadNextPage }: Props ) => {
 
-      <FlatList 
+
+  const isLoading = useRef( false );
+
+  const onScroll = ( event: NativeSyntheticEvent<NativeScrollEvent> ) => {
+    // Evalua si ya se está cargando y no ejecutar otra acción
+    if ( isLoading.current ) return;
+
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    // Evalúa si se ha llegado al final del scroll con un margen de 600px
+    const isEndReached = contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if ( !isEndReached ) return;
+
+    isLoading.current = true;
+
+    // TODO Aquí iría la lógica para cargar más películas
+    console.log( 'Cargar más películas' );
+    loadNextPage?.(); // Llama a la función para cargar la siguiente página si está definida
+    
+  };
+
+
+  return (
+    <View className={ `${ className }` }>
+      { title && <Text className="text-3xl font-bold px-4 mb-2">{ title }</Text> }
+
+      <FlatList
         horizontal
         data={ movies }
-        showsHorizontalScrollIndicator = { false }
-        keyExtractor = { (item) => `${item.id}` }
-        renderItem = { ( {item} ) => <MoviePoster id={item.id} poster = {item.poster} smallPoster/>}
-
-        />
+        showsHorizontalScrollIndicator={ false }
+        keyExtractor={ ( item ) => `${ item.id }` }
+        renderItem={ ( { item } ) => <MoviePoster id={ item.id } poster={ item.poster } smallPoster /> }
+        onScroll={ onScroll }
+      />
     </View>
-  )
-}
+  );
+};
 
 export default MovieHorizontalList;
